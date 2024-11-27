2ea2c6b00a08d33661a7dbda081260c24a365064
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2024-11-26 21:25:46 -0700

    add docs for ki18n

--- src/ki18n/kstringextensions.rs
+++ src/ki18n/kstringextensions.rs
@@ -5,15 +5,32 @@ use cxx_qt_lib::QString;
 
 use super::KLocalizedString;
 
-// Convert this into macros later on with substitutions
+// TODO: Convert this into macros later with substitutions. Can also use variadics.
+
+/// Helper function for [KLocalizedString::ki18n]
+///
+/// Create finalized translated string.
 pub fn i18n(text: &str) -> QString {
     return KLocalizedString::ki18n(text.to_string()).to_qstring();
 }
 
+/// Helper function for [KLocalizedString::ki18nc]
+///
+/// Create finalized translated string with context.
 pub fn i18nc(context: &str, text: &str) -> QString {
     return KLocalizedString::ki18nc(context.to_string(), text.to_string()).to_qstring();
 }
 
+/// Helper function for [KLocalizedString::ki18np]
+///
+/// Create finalized translated string with plural.
+pub fn i18np(singular: &str, plural: &str) -> QString {
+    return KLocalizedString::ki18np(singular.to_string(), plural.to_string()).to_qstring();
+}
+
+/// Helper function for [KLocalizedString::ki18ncp]
+///
+/// Create finalized translated string with context and plural.
 pub fn i18ncp(context: &str, singular: &str, plural: &str) -> QString {
     return KLocalizedString::ki18ncp(
         context.to_string(),
@@ -23,15 +40,32 @@ pub fn i18ncp(context: &str, singular: &str, plural: &str) -> QString {
     .to_qstring();
 }
 
+/// Helper function for [KLocalizedString::ki18nd]
+///
+/// Create finalized translated string from domain.
 pub fn i18nd(domain: &str, text: &str) -> QString {
     return KLocalizedString::ki18nd(domain.to_string(), text.to_string()).to_qstring();
 }
 
+/// Helper function for [KLocalizedString::ki18ndc]
+///
+/// Create finalized translated string from domain with context.
 pub fn i18ndc(domain: &str, context: &str, text: &str) -> QString {
     return KLocalizedString::ki18ndc(domain.to_string(), context.to_string(), text.to_string())
         .to_qstring();
 }
 
+/// Helper function for [KLocalizedString::ki18ndp]
+///
+/// Create finalized translated string from domain with plural.
+pub fn i18ndp(domain: &str, singular: &str, plural: &str) -> QString {
+    return KLocalizedString::ki18ndp(domain.to_string(), singular.to_string(), plural.to_string())
+        .to_qstring();
+}
+
+/// Helper function for [KLocalizedString::ki18ndcp]
+///
+/// Create finalized translated string from domain with context and plural.
 pub fn i18ndcp(domain: &str, context: &str, singular: &str, plural: &str) -> QString {
     return KLocalizedString::ki18ndcp(
         domain.to_string(),
@@ -40,13 +74,4 @@ pub fn i18ndcp(domain: &str, context: &str, singular: &str, plural: &str) -> QSt
         plural.to_string(),
     )
     .to_qstring();
-}
-
-pub fn i18ndp(domain: &str, singular: &str, plural: &str) -> QString {
-    return KLocalizedString::ki18ndp(domain.to_string(), singular.to_string(), plural.to_string())
-        .to_qstring();
-}
-
-pub fn i18np(singular: &str, plural: &str) -> QString {
-    return KLocalizedString::ki18np(singular.to_string(), plural.to_string()).to_qstring();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 // SPDX-FileCopyrightText: 2024 Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-pub mod kconfigwidgets;
+#![doc = include_str!("../README.md")]
 pub mod kcoreaddons;
 pub mod kcrash;
+
+/// KI18n provides functionality for internationalizing user interface text in applications, based on the GNU Gettext translation system.
+/// It provides functionality to use translated strings from the Rust and QML interfaces.
+///
+/// [C++ API](https://api.kde.org/frameworks/ki18n/html/index.html)
 pub mod ki18n;
 pub mod kiconthemes;
