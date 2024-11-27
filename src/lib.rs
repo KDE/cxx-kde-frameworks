1f86247bcd33bd260312226f0a43c454f9810b80
Nicolas Fella <nicolas.fella@gmx.de>
2024-11-27 21:25:39 +0100

    Remove unneeded Qt version check

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 // SPDX-FileCopyrightText: 2024 Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-#[cfg(not(any(cxxqt_qt_version_major = "5", cxxqt_qt_version_major = "6")))]
-compile_error!("cxxqt_qt_version_major must be either \"5\" or \"6\"");
-
 pub mod kconfigwidgets;
 pub mod kcoreaddons;
 pub mod kcrash;
