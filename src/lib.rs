d08af634b9f984654ed9a104678dd5b249cf8b82
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2024-11-26 21:25:46 -0700

    add docs for ki18n

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 // SPDX-FileCopyrightText: 2024 Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
-#[cfg(not(any(cxxqt_qt_version_major = "5", cxxqt_qt_version_major = "6")))]
-compile_error!("cxxqt_qt_version_major must be either \"5\" or \"6\"");
+#![doc = include_str!("../README.md")]
 
 pub mod kcoreaddons;
 pub mod kcrash;
+
+/// KI18n provides functionality for internationalizing user interface text in applications, based on the GNU Gettext translation system.
+/// It provides functionality to use translated strings from the Rust and QML interfaces.
+///
+/// [C++ API](https://api.kde.org/frameworks/ki18n/html/index.html)
 pub mod ki18n;
