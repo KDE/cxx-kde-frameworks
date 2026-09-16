84ca8621eb678978f06a41bb8436bc53ff217afb
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2026-09-15 23:33:26 -0400

    chore: update kapp with kaboutdata

--- examples/kapp/src/main.rs
+++ examples/kapp/src/main.rs
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use cxx_kde_frameworks::kconfigwidgets::kstylemanager;
+use cxx_kde_frameworks::kcoreaddons::{KAboutData, KAboutPerson, License};
 use cxx_kde_frameworks::kcrash::KCrash;
-use cxx_kde_frameworks::ki18n::{self, KLocalizedString};
+use cxx_kde_frameworks::ki18n::{self, KLocalizedString, i18nc};
 use cxx_kde_frameworks::kiconthemes::KIconTheme;
-use cxx_qt_lib::{QGuiApplication, QQuickStyle, QString};
+use cxx_qt_lib::{QGuiApplication, QQuickStyle, QString, QUrl};
 use qtbridge::QApp;
 
 fn main() {
@@ -13,18 +14,41 @@ fn main() {
 
     KIconTheme::init_theme();
     KCrash::initialize();
-    kstylemanager::init_style();
 
     KLocalizedString::set_application_domain("kapp");
-    ki18n::setup_localized_context(&mut app);
-
-    // To associate the executable to the installed desktop file
     QGuiApplication::set_desktop_file_name(&QString::from("org.kde.kapp"));
 
     // To ensure the style is set correctly
+    kstylemanager::init_style(); // OR do it manually as below
     if std::env::var("QT_QUICK_CONTROLS_STYLE").is_err() {
         QQuickStyle::set_style(&QString::from("org.kde.desktop"));
     }
+
+    let mut about_data = KAboutData::from(
+        "kapp",
+        "KApp",
+        "1.0",
+        "Sample Rust Application!",
+        License::GPL_V3,
+    );
+
+    if let Some(about) = about_data.as_mut() {
+        about
+            .add_author(&KAboutPerson::from(
+                "Konqi",
+                "Conqueror",
+                "konqi@kde.org",
+                "kde.org",
+                &QUrl::from("https://konqi.png"),
+            ))
+            .set_translator(
+                &i18nc("NAME OF TRANSLATORS", "Your names"),
+                &i18nc("EMAIL OF TRANSLATORS", "Your emails"),
+            );
+    }
+
+    KAboutData::set_application_data(&about_data);
+    ki18n::setup_localized_context(&mut app);
 
     app.load_qml(include_bytes!("qml/Main.qml"));
     app.run();
--- examples/kapp/src/qml/Main.qml
+++ examples/kapp/src/qml/Main.qml
@@ -6,16 +6,44 @@
 
 import QtQuick
 import QtQuick.Controls
+import QtQuick.Layouts
 import org.kde.ki18n
+import org.kde.kirigami as Kirigami
+import org.kde.kirigamiaddons.formcard as FormCard
 
-ApplicationWindow {
+Kirigami.ApplicationWindow {
+    id: root
+    width: 400
+    height: 400
 
     visible: true
     title: KI18n.i18n("kapp")
 
-    Label {
-        font.pixelSize: 20
-        text: KI18n.i18n("Hello World!")
-        anchors.centerIn : parent
+    Component {
+           id: about
+           FormCard.AboutPage {}
     }
+
+    pageStack.initialPage: Kirigami.Page {
+        ColumnLayout {
+            anchors.fill: parent
+            
+            Label {
+                font.pixelSize: 20
+                text: KI18n.i18n("KDE ❤️ Rust")
+                Layout.alignment: Qt.AlignHCenter 
+            }
+
+            Item {
+                implicitHeight: Kirigami.Units.largeSpacing
+            }
+
+            Button {
+                text: "About"
+                onClicked: root.pageStack.push(about)
+                Layout.alignment: Qt.AlignHCenter 
+            }
+        }
+    }
+
 }
